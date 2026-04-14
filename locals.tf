d3b517346ebe23f82526f85c907e2af6c474f475
John Ajera <37360952+jajera@users.noreply.github.com>
2026-04-15 06:46:35 +1200

    chore: bump version
    cleanup and triggers version bump

--- locals.tf
+++
@@ -1,8 +0,0 @@
-# locals {
-#   s3_bucket_names = {
-#     "web-assets" = "${var.name_prefix}-web-assets-${random_id.s3_bucket_suffix.hex}"
-#     "web-logs"   = "${var.name_prefix}-web-logs-${random_id.s3_bucket_suffix.hex}"
-#   }
-#
-#   # web_domain_name = "web.${var.domain_name}"
-# }
